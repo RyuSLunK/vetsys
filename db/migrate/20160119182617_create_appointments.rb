80ff78cff2a990c4a4c114366d4c5753e2303b31
Thomas Scott Lackey <thomasscottlackey@gmail.com>
2016-01-19 11:55:35 -0800

    Modified Appointment migration
    Date of Visit, Pet and Customer must be non-null.

--- db/migrate/20160119182617_create_appointments.rb
+++ db/migrate/20160119182617_create_appointments.rb
@@ -1,9 +1,9 @@
 class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
-      t.datetime :visit_date
-      t.references :pet, index: true
-      t.references :customer, index: true
+      t.datetime :visit_date, :null => false
+      t.references :pet, index: true, :null => false
+      t.references :customer, index: true, :null => false
       t.boolean :reminder
       t.text :reason
 
