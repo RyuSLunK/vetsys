62d6fcdfc6703f92f9e216abf2c7ee068691b8fc
Thomas Scott Lackey <thomasscottlackey@gmail.com>
2016-01-19 11:47:52 -0800

    Doctor Migration Modified
    Added limit to name string.
    NOTE: need to add validation for years (value between 1-100) and zip
    (should be at most 5 digits) in Doctor model.

--- db/migrate/20160119182321_create_doctors.rb
+++ db/migrate/20160119182321_create_doctors.rb
@@ -1,11 +1,11 @@
 class CreateDoctors < ActiveRecord::Migration
   def change
     create_table :doctors do |t|
-      t.string :name
+      t.string :name, :limit => 35, :null => false
       t.string :address
       t.string :city
       t.string :state
-      t.integer :zip
+      t.string :zip, :limit => 5
       t.string :school
       t.integer :years
 
